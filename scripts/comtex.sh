dc09906f211b80e01e881bd0b014243939873fa1
RUCO13 <ruizoscar.1393@gmail.com>
2022-01-22 17:01:13 -0600

    initial commit

---
+++ scripts/comtex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+dir=$(pwd)
+cd $dir
+
+usage()
+{
+    echo "You select simple option compile"
+    pdflatex -synctex=1 -interaction=nonstopmode -output-directory=out-ruco *.tex
+}
+
+while getopts "bs:" option;do
+    case $option in 
+        b)
+            pdflatex -synctex=1 -interaction=nonstopmode -output-directory=out-ruco *.tex &&
+            echo -e "\n\n\n\t\t\t Compile BIB \n\n\n"
+            bibtex out-ruco/*.aux &&
+            echo -e "\n\n\n"
+            pdflatex -synctex=1 -interaction=nonstopmode -output-directory=out-ruco *.tex
+            ;;
+        s)
+            echo "You select Makeindex option compile"
+            pdflatex -synctex=1 -interaction=nonstopmode -output-directory=out-ruco *.tex
+            makeglossaries  -s  out-ruco/*.ist -t out-ruco/*.glg -o out-ruco/*.gls out-ruco/*.glo
+            pdflatex -synctex=1 -interaction=nonstopmode -output-directory=out-ruco *.tex
+            ;;
+        *)  
+            usage
+            exit;;
+    esac
+done
+
+            
