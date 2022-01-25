2a40fb6dc40ab29b9b2dc0bcb22040c3b7433504
RUCO13 <ruizoscar.1393@gmail.com>
2022-01-25 11:55:17 -0600

    update

--- scripts/remove-bkgrd.sh
+++ scripts/remove-bkgrd.sh
@@ -7,7 +7,7 @@ i=0
 for f in  $imagefiles; do
     ((i=i+1))
     #convert $f -transparent white -fuzz 10% "F${i}.png"
-    convert $f -fuzz 10% -transparent White "${path}/exout-${i}.png"
+    convert $f -fuzz 10% -transparent White "$exout-${i}.png"
     echo "Remove Background to image $f -- > F${i}.png"
 done
 
