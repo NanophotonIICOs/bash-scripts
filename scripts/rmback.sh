ef82652f23eab87146e95da20feb0a3a32d81a64
RUCO13 <ruizoscar.1393@gmail.com>
2022-02-03 17:58:54 -0600

    update rmback script

--- scripts/rmback.sh
+++ scripts/rmback.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 dir=$(pwd)
 cd $dir
-green=$(tput setaf 2)            
-while getopts "p:e:" option
+filelist=$(ls *.eps)
+green=$'\e[1;32m'
+
+
+for file in $filelist
 do
-    case $option in 
-        p)
-            for fig in $OPTARG;
-            do
-                 convert $fig -fuzz %5 -transparent White "${fig%.*}-out.pdf"
-                 echo -e "${green}$fig ---> ${fig%.*}-out.pdf"
-            done
-            ;;
-        *)  
-            exit;;
-    esac
+    if [[ "$file" =~  .*"$1".* ]]; then
+        convert $file -fuzz %5 -transparent White "${file%.*}-out.pdf"
+        echo "$green $file ---> ${file%.*}-out.pdf"
+    fi
 done
 
 
