e3c634eebe9fee94f4bbd3ec60ad599d59f62aaf
RUCO13 <ruizoscar.1393@gmail.com>
2022-04-12 18:20:20 -0500

    Update

--- scripts/remove-bkgrd.sh
+++ scripts/remove-bkgrd.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+green=$'\e[1;32m'
+red=$'\e[0;31m'
+blue=$'\e[0;34m'
+lcyan=$'\e[1;36m'
+yellow=$'\e[1;33m'
+endcolor=$'\e[0m'
+
 dir=$(pwd)
 cd $dir
-
 imagefiles=`ls -- *.png`
 i=0
 for f in  $imagefiles; do
     ((i=i+1))
-    #convert $f -transparent white -fuzz 10% "F${i}.png"
-    convert $f -fuzz 10% -transparent White "exout-${i}.png"
-    echo "Remove Background to image $f -- > exout-${i}.png"
+
+    # to check if already remove image background, this contains wob string
+    if [[ ${f%.*} == *"wob"* ]]; then
+        echo -e "$red $f Already exist!"
+    else
+        convert $f -fuzz 10% -transparent White "${f%.*}-wob.png"
+        echo -e "$green Remove Background to image  ${f%.*} -- > ${f%.*}-wob.png" 
+    fi
 done
 
 
